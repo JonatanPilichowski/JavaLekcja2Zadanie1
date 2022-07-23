02d91eb55d781be0c0be2408004c64ec55190c83
JonatanPilichowski <108926973+JonatanPilichowski@users.noreply.github.com>
2022-07-23 19:39:08 +0200

    Succesfully generated token

--- src/main/java/token.java
+++ src/main/java/token.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class token {
@@ -10,7 +13,7 @@ public class token {
 
     public static void main(String[] args) {
 
-tokenGenerator();
+        tokenGenerator();
         Scanner scanner = new Scanner(System.in);
         int tokenSize = 0;
         boolean result;
@@ -18,7 +21,6 @@ tokenGenerator();
 
         do {
             System.out.println("Dostępne rozmiary tokenów to 5, 10, 15. Podaj wielkość z rozmiarów ");
-
 
 
             tokenSize = scanner.nextInt();
@@ -38,8 +40,20 @@ tokenGenerator();
         } while (!result);
 
 
-        System.out.println("Wybrałeś rozmiar: " + tokenSize);
+        System.out.println("Wybrałeś token o rozmiarze: " + tokenSize + ".");
 
+        List<String> tokenValue = new ArrayList<>();
+
+        for (int i = 0; i < tokenSize; i++) {
+            Random r = new Random();
+
+            genRandom = (char) (r.nextInt(93) + '!');
+
+            tokenValue.add(String.valueOf(genRandom));
+
+        }
+        String listString = String.join("", tokenValue);
+        System.out.println("Wygenerowano token: " + listString);
     }
 
 }
